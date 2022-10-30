bf6966b2e1930ac798bed0fdd74967b907f42aff
doking517 <tsemojust@gmail.com>
2022-10-30 22:20:20 +0100

    first commit message

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    '**/*.{css,html,js}'
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ['**/*.{css,html,js}'],
+	theme: {
+		extend: {},
+		// colors: {
+		// 	govinal: ['#111827'],
+		// },
+	},
+	plugins: [],
+};
